4c825679cfa4d47ee24a0be95630190cf7355c73
Anwit Dahal <dahalanwit0707@gmail.com>
2024-07-21 21:09:44 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require('express')
+const socket= require('socket.io')
+const http=require('http')
+const {Chess}=require('chess.js')
+const app=express();
+
+const server=http.createServer(app)
+const io=socket(server)
+ 
+const chess = new Chess();
+
+let players={}
+let currentPlayer='W'
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello world")
+})
+
+app.listen(3000)
