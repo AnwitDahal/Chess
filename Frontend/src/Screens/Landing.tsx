fb76d89c9f1fa2c6c1e80e695bcf0a25831f7581
AnwitDahal <dahalanwit0707@gmail.com>
2024-07-25 20:33:32 +0545

    Image added in frontend

--- Frontend/src/Screens/Landing.tsx
+++ Frontend/src/Screens/Landing.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <div>
+      <div className="mt-2">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          <div>
+            <img src={"/chessboard.jpg"} alt="" />
+          </div>
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Landing
+export default Landing;
