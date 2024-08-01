aebd93c4f86377c3d4a192b49c59c8ac1fc97484
AnwitDahal <dahalanwit0707@gmail.com>
2024-08-01 21:07:56 +0545

    letters and numbers added

--- Frontend/src/Components/ChessBoard.tsx
+++ Frontend/src/Components/ChessBoard.tsx
@@ -18,57 +18,85 @@ const ChessBoard = ({
   socket: WebSocket;
 }) => {
   const [from, setFrom] = useState<null | Square>(null);
+  
+  const letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+  const numbers = [8, 7, 6, 5, 4, 3, 2, 1];
+
   return (
-    <div className="">
-      {board.map((row, i) => {
-        return (
-          <div key={i} className="flex">
-            {row.map((square, j) => {
-              const squareRepresentation = (String.fromCharCode(97 + (j % 8)) +
-                "" +
-                (8 - i)) as Square;
-              return (
-                <div
-                  onClick={() => {
-                    if (!from) {
-                      setFrom(squareRepresentation);
-                    } else {
-                      socket.send(
-                        JSON.stringify({
-                          type: MOVE,
-                          payload: {
-                            move: {
-                              from,
-                              to: squareRepresentation,
+    <div className="flex">
+      <div className="flex flex-col text-white">
+        {numbers.map((number, i) => (
+          <div key={i} className="h-16 flex items-center justify-center mr-2">
+            {number}
+          </div>
+        ))}
+      </div>
+      <div className="">
+        {board.map((row, i) => {
+          return (
+            <div key={i} className="flex">
+              {row.map((square, j) => {
+                const squareRepresentation = (String.fromCharCode(97 + (j % 8)) +
+                  "" +
+                  (8 - i)) as Square;
+                return (
+                  <div
+                    onClick={() => {
+                      if (!from) {
+                        setFrom(squareRepresentation);
+                      } else {
+                        socket.send(
+                          JSON.stringify({
+                            type: MOVE,
+                            payload: {
+                              move: {
+                                from,
+                                to: squareRepresentation,
+                              },
                             },
-                          },
-                        })
-                      );
-                      setFrom(null);
-                      chess.move({
-                        from,
-                        to: squareRepresentation,
-                      });
-                      setBoard(chess.board());
-                    }
-                  }}
-                  key={j}
-                  className={`w-16 h-16 ${
-                    (i + j) % 2 === 0 ? "bg-white" : "bg-green-700"
-                  }`}
-                >
-                  <div className="flex justify-center w-full h-full">
-                    <div className="h-full flex justify-center flex-col">
-                      {/* {square ? square.type : ""} */}
-                      {square ? <img className="" src={`/${square?.color==="b"? square.type : `${square?.type?.toUpperCase()}w`}.svg`}/>:null}
+                          })
+                        );
+                        setFrom(null);
+                        chess.move({
+                          from,
+                          to: squareRepresentation,
+                        });
+                        setBoard(chess.board());
+                      }
+                    }}
+                    key={j}
+                    className={`w-16 h-16 ${
+                      (i + j) % 2 === 0 ? "bg-white" : "bg-green-700"
+                    }`}
+                  >
+                    <div className="flex justify-center w-full h-full">
+                      <div className="h-full flex justify-center flex-col">
+                        {square ? (
+                          <img
+                            className=""
+                            src={`/${
+                              square?.color === "b"
+                                ? square.type
+                                : `${square?.type?.toUpperCase()}w`
+                            }.svg`}
+                          />
+                        ) : null}
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
+                );
+              })}
+            </div>
+          );
+        })}
+        <div className="flex">
+          {letters.map((letter, i) => (
+            <div key={i} className="w-16 h-8 flex items-center justify-center text-white">
+              {letter}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
